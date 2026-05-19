5bdae7af1ea1771e88ab870cc17b13c01e901951
o9 <shno59.9009@gmail.com>
2026-05-19 18:43:53 +0300

    Update and rename vesktopProtocol.ts to nunbopProtocol.ts

--- src/main/nunbopProtocol.ts
+++ src/main/nunbopProtocol.ts
@@ -1,13 +1,13 @@
 /*
- * Vesktop, a desktop app aiming to give you a snappier Discord Experience
- * Copyright (c) 2025 Vendicated and Vesktop contributors
+ * Nunbop, a desktop app aiming to give you a snappier Discord Experience
+ * Copyright (c) 2026 Vendicated and Nunbop contributors
  * SPDX-License-Identifier: GPL-3.0-or-later
  */
 
 import { app, protocol } from "electron";
 
 import { handleVesktopAssetsProtocol } from "./userAssets";
-import { handleVesktopStaticProtocol } from "./vesktopStatic";
+import { handleNunbopStaticProtocol } from "./nunbopStatic";
 
 app.whenReady().then(() => {
     protocol.handle("nunbop", async req => {
@@ -17,7 +17,7 @@ app.whenReady().then(() => {
             case "assets":
                 return handleVesktopAssetsProtocol(url.pathname, req);
             case "static":
-                return handleVesktopStaticProtocol(url.pathname, req);
+                return handleNunbopStaticProtocol(url.pathname, req);
             default:
                 return new Response(null, { status: 404 });
         }
