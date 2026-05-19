c406a08fc521d72bb60b44bee4e934f69e2edee9
o9 <shno59.9009@gmail.com>
2026-05-19 18:29:48 +0300

    Update sandboxFix.mjs

--- scripts/build/sandboxFix.mjs
+++ scripts/build/sandboxFix.mjs
@@ -1,5 +1,5 @@
 /*
- * Vesktop, a desktop app aiming to give you a snappier Discord Experience
+ * Nunbop, a desktop app aiming to give you a snappier Discord Experience
  * Copyright (c) 2023 Vendicated and Vencord contributors
  * SPDX-License-Identifier: GPL-3.0-or-later
  */
@@ -52,7 +52,7 @@ SCRIPT_DIR="$( cd "$( dirname "\${BASH_SOURCE[0]}" )" && pwd )"
 IS_STEAMOS=0
 
 if [[ "$SteamOS" == "1" && "$SteamGamepadUI" == "1" ]]; then
-    echo "Running Equibop on SteamOS, disabling sandbox"
+    echo "Running Nunbop on SteamOS, disabling sandbox"
     IS_STEAMOS=1
 fi
 
