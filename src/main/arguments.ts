d24f383cd2fc2228b0314112b42d4eb043bca9ee
o9 <shno59.9009@gmail.com>
2026-05-19 18:39:12 +0300

    Update arguments.ts

--- src/main/arguments.ts
+++ src/main/arguments.ts
@@ -1,5 +1,5 @@
 /*
- * Vesktop, a desktop app aiming to give you a snappier Discord Experience
+ * Nunbop, a desktop app aiming to give you a snappier Discord Experience
  * Copyright (c) 2023 Vendicated and Vencord contributors
  * SPDX-License-Identifier: GPL-3.0-or-later
  */
@@ -10,7 +10,7 @@ import { STATIC_DIR } from "shared/paths";
 
 import { State } from "./settings";
 import { makeLinksOpenExternally } from "./utils/makeLinksOpenExternally";
-import { loadView } from "./vesktopStatic";
+import { loadView } from "./nunbopStatic";
 
 let argumentsWindow: BrowserWindow | null = null;
 
